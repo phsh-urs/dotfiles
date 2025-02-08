b4f2dc10677ff9ef4a398e38304f626b241a8bf5
Pedro Ursino <phalmursino@gmail.com>
2025-02-08 20:06:53 -0300

    INSTALL.sh: fix typos and comment zsh config files

--- INSTALL.sh
+++ INSTALL.sh
@@ -34,44 +34,15 @@ if ! test -f "$HOME/.config/redshift/redshift.conf"; then
   mkdir -p "$HOME"/.config/redshift/
   ln -s "$HOME"/dotfiles/redshift.conf "$HOME"/.config/redshift/redshift.conf
 fi
-if ! test -f "$HOME/.zshrc"; then
-  ln -s "$HOME"/dotfiles/zshrc "$HOME"/.zshrc
-fi
-if ! test -f "$HOME/.zproflie"; then
-  ln -s "$HOME"/dotfiles/zproflie "$HOME"/.zproflie
-fi
 
-# vim
-# mkdir -p "$HOME"/.vim/tmp/
-# mkdir -p "$HOME"/.vim/after/
-# mkdir -p "$HOME"/.vim/tags/
-# if ! test -d "$HOME/.vim/after/ftplugin"; then
-#   ln -s "$HOME"/dotfiles/vim/after/ftplugin "$HOME"/.vim/after/ftplugin
-# fi
-# if ! test -d "$HOME/.vim/after/syntax"; then
-#   ln -s "$HOME"/dotfiles/vim/after/syntax "$HOME"/.vim/after/syntax
-# fi
-# if ! test -d "$HOME/.vim/colors"; then
-#   ln -s "$HOME"/dotfiles/vim/colors "$HOME"/.vim/colors
-# fi
-# if ! test -d "$HOME/.vim/doc"; then
-#   ln -s "$HOME"/dotfiles/vim/doc "$HOME"/.vim/doc
-# fi
-# if ! test -d "$HOME/.vim/snips"; then
-#   ln -s "$HOME"/dotfiles/vim/snips "$HOME"/.vim/snips
-# fi
-# # stopgap
-# if ! test -d "$HOME/.vim/spell"; then
-#   if test -d "$HOME"/dotfiles/vim/spell; then
-#     ln -s "$HOME"/dotfiles/vim/spell "$HOME"/.vim/spell
-#   fi
-# fi
-# if ! test -f "$HOME/.vim/coc-settings.json"; then
-#   ln -s "$HOME"/dotfiles/vim/coc-settings.json "$HOME"/.vim/coc-settings.json
-# fi
-if ! test -f "$HOME/.vim/vimrc"; then
-  ln -s "$HOME"/dotfiles/vim/vimrc "$HOME"/.vim/vimrc
-fi
+# zsh (not being used)
+#  if ! test -f "$HOME/.zshrc"; then
+#  ln -s "$HOME"/dotfiles/zshrc "$HOME"/.zshrc
+#fi
+#if ! test -f "$HOME/.zprofile"; then
+#  ln -s "$HOME"/dotfiles/zprofile "$HOME"/.zprofile
+#fi
+
 if ! test -f "$HOME/.vimrc"; then
-  ln -s "$HOME"/dotfiles/vim/vimrc "$HOME"/.vimrc
+  ln -s "$HOME"/dotfiles/vimrc "$HOME"/.vimrc
 fi
