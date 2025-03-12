d48d4b1e1f2f840415db710665a7b70243c870e3
phsh <phalmursio@gmail.com>
2025-03-12 19:16:25 -0300

    edit(xbanish): stop sending cursor to corner

--- sh-scripts/startup.sh
+++ sh-scripts/startup.sh
@@ -3,6 +3,6 @@
 xss-lock -n ~/dotfiles/sh-scripts/lock-warning.sh -- ~/dotfiles/sh-scripts/fuzzy-lock.sh &
 
 picom -b --no-fading-openclose
-xbanish -m se -t 4 -s &
+xbanish -t 4 -s &
 ~/dotfiles/sh-scripts/redshift.sh
 dunst &
